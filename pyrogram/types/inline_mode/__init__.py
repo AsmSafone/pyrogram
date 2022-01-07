075c76e354734c2b6d8524bed9efeb334829d52a
Dan <14043624+delivrance@users.noreply.github.com>
2022-01-07 10:18:51 +0100

    Various improvements

--- pyrogram/types/inline_mode/__init__.py
+++ pyrogram/types/inline_mode/__init__.py
@@ -21,6 +21,7 @@ from .inline_query import InlineQuery
 from .inline_query_result import InlineQueryResult
 from .inline_query_result_animation import InlineQueryResultAnimation
 from .inline_query_result_article import InlineQueryResultArticle
+from .inline_query_result_audio import InlineQueryResultAudio
 from .inline_query_result_photo import InlineQueryResultPhoto
 from .inline_query_result_video import InlineQueryResultVideo
 from .inline_query_result_audio import InlineQueryResultAudio
